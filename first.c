d32bc15c0490d3623797fc952b485d9b4862a667
Oshini Jayaweera <oshinividj@gmail.com>
2025-01-30 00:18:05 +0100

    Add files via upload

---
+++ first.c
@@ -0,0 +1,23 @@
+/* 
+Program: first.c
+Course : SOL1 SS2024
+Author : Oshini Jayaweera
+Date   : 23.04.2024 
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("According to \"Murphyís law\" (https://en.wikipedia.org/wiki/Murphy%%27s_law)\n");
+    printf("from 1949 we believe \"what can go wrong will go wrong sometimes\".\n");
+    printf("For better programming skills and less errors\n");
+    printf("we need to spend 50 % of our time for practical exercising")
+    printf(With German letters you may have your trouble, too, like in
+    printf("h®ort mit Spaﬂ viel ®uber sch®one Dinge".
+
+            
+    return 0;
+            
+}
+
